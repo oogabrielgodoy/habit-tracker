8f23e4359b6edff6aa932ff78fe2c2b5a4f737e5
ogabrielgodoy <ogabrielgodoy@hotmail.com>
2026-01-16 19:46:27 -0300

    feat: setup tailwindcss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,15 @@
-<h1>Welcome home page</h1>
-<p>
-    Olá {{ $name }}
-</p>
-<p>
-    Seus habitos são
-</p>
-<u>
-    @foreach($habits as $habit)
-    <li>
-        {{ $habit }}
-    </li>
-    @endforeach
-</u>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>{{ config('app.name') }}</title>
+  @vite('resources/css/app.css')
+</head>
+
+<body>
+  <h1 class="text-3x1 font-bold underline text-green-500">Hello world </h1>
+</body>
+
+</html>
